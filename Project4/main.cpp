5357b1c8c07ca7dd427b0629d4c15d4e4b3bec96
riadmammadov <riad.mammadov@city.ac.uk>
2023-06-25 23:12:03 +0100

    Add project files.

---
+++ Project4/main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+
+using std::string;
+
+class student {
+
+	int studentN;
+	string name;
+	int engm, mathsm, scim, compscim;
+	double average;
+	char grade;
+public:
+	void calculateAverage();
+	void getData();
+	void studentNumber();
+	void showData() const;
+};
+
+void student::calculateAverage() {
+
+	average = (engm + mathsm + scim + compscim) / 4;
+	if (average >= 90)
+		grade = 'A';
+	if (average >= 75)
+		grade = 'B';
+	if (average >= 55)
+		grade = 'C';
+}
+
+int main() {
+
+
+}
