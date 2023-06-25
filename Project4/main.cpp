0b75cea6b4e2da90d91657d3c16c9c8f2b71a3c5
riadmammadov <riad.mammadov@city.ac.uk>
2023-06-25 23:31:44 +0100

    added functions

--- Project4/main.cpp
+++ Project4/main.cpp
@@ -27,7 +27,40 @@ void student::calculateAverage() {
 		grade = 'B';
 	if (average >= 55)
 		grade = 'C';
+	else
+		grade = 'F';
 } 
+
+void student::getData() {
+
+	std::cout << "Enter student number: " << std::endl;
+	std::cin >> studentN;
+	std::cout << "Enter student name: " << std::endl;
+	std::cin.ignore();
+	std::cin >> name;
+	std::cout << "Enter students marks out of 100" << std::endl;
+	std::cout << "English Marks: ";
+	std::cin >> engm;
+	std::cout << "Maths marks: ";
+	std::cin >> mathsm;
+	std::cout << "Science marks: ";
+	std::cin >> scim;
+	std::cout << "Computer Science marks: ";
+	std::cin >> compscim;
+	calculateAverage();
+
+}
+
+void student::showData() const {
+
+	std::cout << "Student Number: " << studentN << std::endl;
+	std::cout << "Student Name: " << name << std::endl;
+	std::cout << "English Mark: " << engm << std::endl;
+	std::cout << "Maths Mark: " << mathsm << std::endl;
+	std::cout << "Science Mark: " << scim << std::endl;
+	std::cout << "Computer Science Mark: " << compscim << std::endl;
+
+}
 
 int main() {
 
